8863e5f3b0069b364a873a9ead8f9801462e899e
LatinaAtanasova <32022885+LatinaAtanasova@users.noreply.github.com>
2018-10-02 21:17:53 +0300

    Add files via upload

---
+++ SIS.HTTP/Extensions/StringExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SIS.HTTP.Extensions
+{
+   public static class StringExtensions
+    {
+        public static string Capitalize(this string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                throw new ArgumentException($"{nameof(input)} cannot be null!");
+            }
+            string text = input.ToLower();
+            return char.ToUpper(text[0]) + text.Substring(1);
+        }
+    }
+}
---
+++ SIS/Launcher.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using SIS.HTTP.Enums;
+using SIS.HTTP.Extensions;
+using SIS.WebServer;
+using SIS.WebServer.Routing;
+
+namespace SIS.Demo
+{
+    public class Launcher
+    {
+        public static void Main(string[] args)
+        {
+            ServerRoutingTable serverRoutingTable = new ServerRoutingTable();
+
+            serverRoutingTable.Routes[HttpRequestMethod.Get]["/"] = request => new HomeController().Index();
+
+            Server server = new Server(8000, serverRoutingTable);
+
+            server.Run();
+        }
+    }
+}
